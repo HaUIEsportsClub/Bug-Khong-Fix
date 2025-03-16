d93275d3e46ec5963d527901bc0b11fb8667a617
AcXuanIT <phucxuanit75@gmail.com>
2025-03-16 09:04:10 +0700

    MenuSetting

--- BugKhongFix/Assets/Script/UI/UISetting.cs
+++ BugKhongFix/Assets/Script/UI/UISetting.cs
@@ -10,13 +10,16 @@ public class UISetting : MonoBehaviour
     [SerializeField] private Button btnContinue;
     [SerializeField] private Button btnAgain;
     [SerializeField] private Button btnBack;
-
+    [SerializeField] private UISetting1 uisetting1;
     private void Start()
     {
         btnContinue.onClick.AddListener(delegate
         {
             GameController.Instance.GameContinue();
             menuSetting.SetActive(false);
+            uisetting1.isPause = false;
+            uisetting1.checkPause();
+
         });
         btnAgain.onClick.AddListener(delegate
         {
--- BugKhongFix/Assets/Script/UI/UISetting1.cs
+++ BugKhongFix/Assets/Script/UI/UISetting1.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,13 +8,28 @@ public class UISetting1 : MonoBehaviour
 {
     [SerializeField] private GameObject menuSetting;
     [SerializeField] private Button btnPause;
-
+    [SerializeField] private Sprite imagePause;
+    [SerializeField] private Sprite imageContinue;
+    public bool isPause;
     private void Start()
     {
         btnPause.onClick.AddListener(delegate
         {
             GameController.Instance.GamePause();
             menuSetting.SetActive(true);
+            isPause = true;
         });
+    }
+    private void Update()
+    {
+        checkPause();  
+    }
+    public void checkPause()
+    {
+
+        if (!isPause)
+            btnPause.GetComponent<Image>().sprite = imageContinue;
+        else
+            btnPause.GetComponent<Image>().sprite = imagePause;
     }
 }
